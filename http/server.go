bb508227d9ac59fdff5b138f07a3e0ec7cc8cb41
Daniel Graña <dangra@gmail.com>
2023-04-24 12:17:31 -0300

    Log remote address of the stream client (#309)

--- http/server.go
+++ http/server.go
@@ -454,8 +454,8 @@ func (s *Server) handlePostStream(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("%s: stream connected", litefs.FormatNodeID(s.store.ID()))
-	defer log.Printf("%s: stream disconnected", litefs.FormatNodeID(s.store.ID()))
+	log.Printf("%s: stream connected (%s)", litefs.FormatNodeID(s.store.ID()), r.RemoteAddr)
+	defer log.Printf("%s: stream disconnected (%s)", litefs.FormatNodeID(s.store.ID()), r.RemoteAddr)
 
 	serverStreamCountMetric.Inc()
 	defer serverStreamCountMetric.Dec()
